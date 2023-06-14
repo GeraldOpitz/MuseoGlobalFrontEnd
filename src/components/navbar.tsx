9ffde809c2bf3fe36d6d16d621e88fe523713385
GeraldOpitz <geraldopitz@gmail.com>
2023-06-14 17:40:51 -0400

    login funcional

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
 import { AppBar, Toolbar, Typography, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
+import LogoutButton from './logoutButton';
 
 const useStyles = makeStyles((theme) => ({
   navbar: {
@@ -38,6 +39,7 @@ const Navbar = () => {
         <Typography component={Link} to="/aboutUs" variant="h6" className={classes.navbarText}>
           Sobre nosotros
         </Typography>
+        <LogoutButton/>
       </Toolbar>
     </AppBar>
   );
